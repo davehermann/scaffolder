91d34ab696c5d46683a599edffdb7eb0b9d23f37
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2020-08-11 08:15:22 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    ignorePatterns: [`dist/`],
+    "parser": `@typescript-eslint/parser`,
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": `plugin:@typescript-eslint/recommended`,
+    "parserOptions": {
+        "sourceType": `module`,
+        "ecmaVersion": 8,
+    },
+    "rules": {
+        /* Fall 2018: make sure to copy any changes to sub-eslintrc.json as eslint-plugin-vue
+        is prevent inheritance of a number of style */
+        "indent": [`error`, 4, { "SwitchCase": 1 }],
+        "linebreak-style": [`error`, `unix`],
+        "no-extra-boolean-cast": [`off`],
+        "no-unused-vars": [`warn`],
+        "quotes": [`warn`, `backtick`],
+        "semi": [`error`, `always`],
+        "no-console": [`warn`]
+    }
+};
